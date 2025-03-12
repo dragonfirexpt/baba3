57e1b8389e68784e29076c11b90c8e07296472ab
dragonfirexpt <nottyastobusiness@gmail.com>
2025-03-12 19:20:05 -0100

    Update bau.lua

--- bau.lua
+++ bau.lua
@@ -12348,7 +12348,7 @@ local function C_250()
 local script = G2L["250"];
 	local userInputService = game:GetService("UserInputService")
 	
-	local npc = game.StarterPlayer:FindFirstChild("StarterCharacter") -- Change "NPC" to your NPC's name
+	local npc = game.Workspace.NPCs:FindFirstChild("GasStation") -- Change "NPC" to your NPC's name
 	if not npc then
 		warn("NPC not found in StarterPlayer!")
 		return
